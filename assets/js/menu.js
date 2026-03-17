e4dacdb46a55a7996948c4e15e9d12f4a8942894
Yony Zarate <yony.zarate96@gmail.com>
2026-03-17 10:09:34 -0400

    add:agregue estilos para reponsive

---
+++ assets/js/menu.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // alert("llego a menu");
+  let button = document.querySelector(".layout__menu--toggle");
+  let icon_bars = document.querySelector(".layout__menu--toggle .fa-bars");
+  let icon_xmark = document.querySelector(".layout__menu--toggle .fa-xmark");
+  let aside = document.querySelector(".layout__aside");
+
+  button.addEventListener("click", (event) => {
+    // alert("click en el boton");
+    let visible = document.querySelector(".layout__aside--visible");
+
+    if (!visible) {
+      aside.classList.add("layout__aside--visible");
+      icon_bars.style.opacity = 0;
+      icon_xmark.style.opacity = 1;
+    } else {
+      aside.classList.remove("layout__aside--visible");
+      icon_bars.style.opacity = 1;
+      icon_xmark.style.opacity = 0;
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    let size = parseInt(document.body.clientWidth);
+
+    if (size <= 1060) {
+      aside.classList.remove("layout__aside--visible");
+
+      icon_bars.style.opacity = 1;
+      icon_xmark.style.opacity = 0;
+    }
+  });
+});
